7acbf67ce98f0e7850edea0fdc1d3bbf48eed010
mountaintop1 <33271138+mountaintop1@users.noreply.github.com>
2023-08-01 22:33:00 -0600

    create user model with test

--- app/core/models.py
+++ app/core/models.py
@@ -1,4 +1,38 @@
-from django.db import models  # noqa
+"""
+Database models
+"""
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
+    PermissionsMixin
 
 
-# Create your models here.
+class UserManager(BaseUserManager):
+    """User manager"""
+    def create_user(self, email, password=None, **extra_fields):
+        """Creates and saves a new user"""
+        if not email:
+            raise ValueError('Users must have an email address')
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)  # using=self._db is required by Django
+        return user
+
+    def create_superuser(self, email, password):
+        """Creates and saves a new superuser"""
+        user = self.create_user(email, password)
+        user.is_staff = True  # is_staff is created by PermissionsMixin
+        user.is_superuser = True  # is_superuser is created by PermissionsMixin
+        user.save(using=self._db)  # using=self._db is required by Django
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """User model that supports using email instead of username"""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'  # USERNAME_FIELD is required by Django
